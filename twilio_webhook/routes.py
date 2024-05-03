ea8139dae9b40abd21bd7b78ec8c9934b3f6ce13
Akash Kahalkar <akash.kahalkar.08@gmail.com>
2024-05-03 15:31:09 +0530

    uncommented test code

--- twilio_webhook/routes.py
+++ twilio_webhook/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request
 import validators
-#from handler import URLHandler
+from handler import URLHandler
 from twilio_webhook.twilio_message_handler import TwilioMessageHandler
 
 twilio_api = Blueprint('twilio', __name__, url_prefix='/twilio')
@@ -11,13 +11,13 @@ def checkServer():
 
 @twilio_api.route('/whatsapp', methods=['GET', 'POST'])
 def handle_whatsapp_webhook():
-    return "<H3>passed</H3>"
-    # message_handler = TwilioMessageHandler()
-    # error_message = message_handler.get_error_message("Arrr! I be at yer service, matey! Unfortunately,\n I can fetch links from the seven seas like YouTube or Instagram or X or terabox. 🏴‍☠️")
-    # receivedUrl = str(request.values.get("Body"))
-    # if not validators.url(receivedUrl):
-    #     return error_message
-    # downloadLink = URLHandler().handle(receivedUrl)
-    # if downloadLink is None:
-    #     return error_message
-    # return message_handler.get_message(downloadLink)
+    
+    message_handler = TwilioMessageHandler()
+    error_message = message_handler.get_error_message("Arrr! I be at yer service, matey! Unfortunately,\n I can fetch links from the seven seas like YouTube or Instagram or X or terabox. 🏴‍☠️")
+    receivedUrl = str(request.values.get("Body"))
+    if not validators.url(receivedUrl):
+        return error_message
+    downloadLink = URLHandler().handle(receivedUrl)
+    if downloadLink is None:
+        return error_message
+    return message_handler.get_message(downloadLink)
