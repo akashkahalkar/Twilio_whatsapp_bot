bbd8c1d7d965528c95eed93726efcd9ec2aaae87
Akash Kahalkar <akash.kahalkar.08@gmail.com>
2024-05-03 18:57:23 +0530

    added bulk downloader

--- shortcuts/routes.py
+++ shortcuts/routes.py
@@ -1,7 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from flask import request
+from downloader.teradownloader import TeraDownloader
 from handler import URLHandler
 import validators
+import re
 
 shortcuts = Blueprint('shortcuts', __name__, url_prefix='/shortcuts')
 
@@ -16,6 +18,29 @@ def get_download_link():
         return error
     return str(download_link)
 
+@shortcuts.route('/bulk', methods=['POST', 'GET'])
+def get_urls():
+    request_json = request.get_json()
+
+# Check if 'data' key exists in the JSON and if the request has JSON data
+    if request_json and 'data' in request_json:
+        url_pattern = r"https?://\S+"
+        response_raw = str(request_json['data'])
+        urls = re.findall(url_pattern, response_raw) 
+        clean = []
+        for url in urls:
+            cleaned_url = re.sub(r'(\\n\d*|\\n\')+?$', '', url)
+            clean.append(cleaned_url)
+        
+        dlinks = TeraDownloader().bulk_fetch_download_links(clean)
+        #VideoDownloader().download_videos(dlinks)
+
+        data = {
+            "urls": dlinks
+        }
+        return jsonify(data)
+    else:
+        return "Error"
 
 
 
