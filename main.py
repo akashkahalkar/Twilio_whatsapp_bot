5730c4f8b010d7da0a82972cb5bfc36a7715b3b6
Akash Kahalkar <akash.kahalkar.08@gmail.com>
2024-05-03 15:28:20 +0530

    testing for wrong imports

--- main.py
+++ main.py
@@ -3,18 +3,13 @@ from shortcuts.routes import shortcuts
 from twilio_webhook.routes import twilio_api
 import os
 
-def create_app():
-    app = Flask(__name__)
-    
-    app.register_blueprint(shortcuts)
-    app.register_blueprint(twilio_api)
+app = Flask(__name__)
+app.register_blueprint(shortcuts)
+app.register_blueprint(twilio_api)
 
-    @app.route('/', methods=['GET', 'POST'])
-    def _welcome():
-        return "<B>Ahoy, landlubber!</B> <br><p>What be yer business sailin' these treacherous seas?<p>"
-    
-    return app
+@app.route('/', methods=['GET', 'POST'])
+def _welcome():
+    return "<B>Ahoy, landlubber!</B> <br><p>What be yer business sailin' these treacherous seas?<p>"
 
 if __name__ == "__main__":
-    app = create_app()
     app.run(host='0.0.0.0',port=os.environ.get("PORT", 5000),use_reloader=True,threaded=True)
