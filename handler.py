74c892b9958bf6526193403b55edb8b7eb72cfad
Akash Kahalkar <akash.kahalkar.08@gmail.com>
2024-05-17 19:48:13 +0530

    added supported domain for terabox

--- handler.py
+++ handler.py
@@ -7,12 +7,14 @@ class URLHandler:
     def handle(self, url):
         # it should be moved to common class where others can also use it?
         all_supported_domain = {'instagram.com', 'x.com', 'youtube.com', 'teraboxapp.com'}
-        ytdlp_suported_domain = {'x.com', 'youtube.com'}
+        ytdlp_suported_domain = {'youtube.com'}
+        tera_supported_links = {'1024terabox.com', 'teraboxapp.com'}
+
 
         if any(domain in url for domain in all_supported_domain):
             if any(domain in url for domain in ytdlp_suported_domain):
                 return YTLoader().getUrl(url)
-            elif 'teraboxapp.com' in url:
+            elif any(domain in url for domain in tera_supported_links):
                 return TeraDownloader().get_fast_download_link(url)
             elif 'instagram.com' in url:
                 result = InstaDownloader().get_reel_download_link(url)
