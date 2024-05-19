8eb33873741fc67c2d1d614dfa876e9bce1d85e7
Akash Kahalkar <akash.kahalkar.08@gmail.com>
2024-05-19 23:46:07 +0530

    added validation check

--- main.py
+++ main.py
@@ -9,7 +9,22 @@ app.register_blueprint(twilio_api)
 
 @app.route('/', methods=['GET', 'POST'])
 def _welcome():
-    return "<B>Ahoy, landlubber!</B> <br><p>What be yer business sailin' these treacherous seas?<p>"
+    return _run_validation_check()
+
+def _run_validation_check():
+    download_domain = os.environ.get("DOWNLOAD_DOMAIN")
+    fast_download_domain = os.environ.get("FAST_DOWNLOAD_DOMAIN")
+    base_url = os.environ.get("TERA_BASE_URL")
+    key = os.environ.get("TERA_KEY")
+    link_param = os.environ.get("LINK_PARAM")
+
+    values = [download_domain, fast_download_domain, base_url, key, link_param]
+    variable_names = ['download_domain', 'fast_download_domain', 'base_url', 'key', 'link_param']
+
+    for name, value in zip(variable_names, values):
+        if value is None:
+            return (f"validation failed for '{name}' ")
+    return "validation check passed!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=os.environ.get("PORT", 5000),use_reloader=True,threaded=True)
