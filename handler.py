9b61b6108c4da72107db02293ddfe9969ff5722c
Akash Kahalkar <akash.kahalkar.08@gmail.com>
2024-06-05 23:23:54 +0530

    changed logic for cmparing tera urls

--- handler.py
+++ handler.py
@@ -6,15 +6,13 @@ import os
 #handle should get a valid url hence the url should be check before this.
 class URLHandler:
     def handle(self, url):
-        # it should be moved to common class where others can also use it?
-        
-        tera_domain_list = os.environ.get("TERA_DOMAINS").split(sep=',')
+        tera_keywords = os.environ.get("TERA_DOMAINS")
         yt_domain_list = os.environ.get("YT_DOMAINS").split(sep=',')
         insta_domain_list = os.environ.get("INSTA_DOMAIN").split(sep=',')
        
         if any(domain in url for domain in yt_domain_list):
             return YTLoader().getUrl(url)
-        elif any(domain in url for domain in tera_domain_list):
+        elif tera_keywords in url:
             return TeraDownloader().get_fast_download_link(url)
         elif any(domain in url for domain in insta_domain_list):
             result = InstaDownloader().download(url)
